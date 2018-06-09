549b874504cd5e71ee68aeed4ce6b49b19631b5c
GabrielCostanzo <gabriel.costanzo@ttu.edu>
2018-06-09 00:57:21 -0500

    serperated json serialization into new module

--- integration.py
+++ integration.py
@@ -13,6 +13,8 @@ import block_verification
 import gzip
 import pymysql
 
+import json_serialize
+
 def print_block(block_object):
 	print("GEN_BLOCK:\n\n\n")
 	print("\nblock hash:")
@@ -97,13 +99,6 @@ for i in range(1):
 	alice.create_transaction(bob.serialized_public, 100000, 5)
 	alice.create_transaction(carl.serialized_public, 10, 5)
 	alice.create_transaction(bob.serialized_public, 10, 5)
-	alice.create_transaction(carl.serialized_public, 500, 50)
-	alice.create_transaction(bob.serialized_public, 10, 5)
-	alice.create_transaction(carl.serialized_public, 90000, 5)
-	alice.create_transaction(bob.serialized_public, 10000000, 600)
-	alice.create_transaction(carl.serialized_public, 10, 3)
-	alice.create_transaction(bob.serialized_public, 10, 5)
-	alice.create_transaction(carl.serialized_public, 10, 5)
 
 test_gen_block = chain.genesis_block(alice.serialized_public, 0)
 test_reg_block = chain.block(test_gen_block, alice.pending_output_transactions, alice.serialized_public, 0)
@@ -143,60 +138,32 @@ def insert_block(block_obj):
 	print(block_obj.nonce)
 	print(block_obj.timestamp)
 """
-def transaction_to_json(transaction_obj):
 
-	if transaction_obj.status == "coinbase":
-		print(json.dumps({"sender_sig": transaction_obj.sender_sig, "transaction_data": transaction_obj.transaction_data,
-		"sender_public_key": transaction_obj.sender_public_key.decode('UTF-8'), "receiver_public_key": transaction_obj.receiver_public_key.decode('UTF-8'), "input_amount": transaction_obj.input_amount, "fees": transaction_obj.fees,
-		"output_amount": transaction_obj.output_amount, "input_transactions": transaction_obj.input_transactions, "status": transaction_obj.status, "txid": transaction_obj.txid, "timestamp": str(transaction_obj.timestamp)},
-		sort_keys=True, indent=4, separators=(',', ': ')))
-
-		json_transaction = json.dumps({"sender_sig": transaction_obj.sender_sig, "transaction_data": transaction_obj.transaction_data,
-		"sender_public_key": transaction_obj.sender_public_key.decode('UTF-8'), "receiver_public_key": transaction_obj.receiver_public_key.decode('UTF-8'), "input_amount": transaction_obj.input_amount, "fees": transaction_obj.fees,
-		"output_amount": transaction_obj.output_amount, "input_transactions": transaction_obj.input_transactions, "status": transaction_obj.status, "txid": transaction_obj.txid, "timestamp": str(transaction_obj.timestamp)})
-	else:
-		print(json.dumps({"sender_sig": transaction_obj.sender_sig.decode('latin1'),  "transaction_data": transaction_obj.transaction_data.decode('latin1'),
-		"sender_public_key": transaction_obj.sender_public_key.decode('UTF-8'), "receiver_public_key": transaction_obj.receiver_public_key.decode('UTF-8'), "input_amount": transaction_obj.input_amount, "fees": transaction_obj.fees,
-		"output_amount": transaction_obj.output_amount, "input_transactions": transaction_obj.input_transactions, "status": transaction_obj.status, "txid": transaction_obj.txid.decode('UTF-8'), "timestamp": str(transaction_obj.timestamp)},
-		sort_keys=True, indent=4, separators=(',', ': ')))
-
-		json_transaction = json.dumps({"sender_sig": transaction_obj.sender_sig.decode('latin1'),  "transaction_data": transaction_obj.transaction_data.decode('latin1'),
-		"sender_public_key": transaction_obj.sender_public_key.decode('UTF-8'), "receiver_public_key": transaction_obj.receiver_public_key.decode('UTF-8'), "input_amount": transaction_obj.input_amount, "fees": transaction_obj.fees,
-		"output_amount": transaction_obj.output_amount, "input_transactions": transaction_obj.input_transactions, "status": transaction_obj.status, "txid": transaction_obj.txid.decode('UTF-8'), "timestamp": str(transaction_obj.timestamp)},
-		sort_keys=True, indent=4, separators=(',', ': '))
-		
-	return json_transaction
-
-"""
-def genesis_to_json(block_obj):
-	coinbase = pickle.loads(block_obj.coinbase_transaction)
-	coinbase_json = transaction_to_json(coinbase)
-	print(json.dumps({"block_hash": block_obj.block_hash.decode('UTF-8'), "height": block_obj.height,
-	"coinbase_transaction": json.loads(coinbase_json), 
-	"transaction_fees": block_obj.transaction_fees, "block_reward": block_obj.block_reward, "total_output": block_obj.total_output,
-	"previous_block_hash": block_obj.previous_block_hash.decode('UTF-8'), "merkle_root": block_obj.merkle_root.decode('UTF-8'),
-	"nonce": block_obj.nonce.decode('UTF-8'), "timestamp": str(block_obj.timestamp), "transactions": json.loads(coinbase_json), "miner_public_key": block_obj.miner_public_key.decode('UTF-8')},
-	sort_keys=True, indent=4, separators=(',', ': ')))
-"""
-
-def block_to_json(block_obj):
-	coinbase = pickle.loads(block_obj.coinbase_transaction)
-	coinbase_json = transaction_to_json(coinbase)
-	json_transactions = []
-
-	for i in block_obj.transactions:
-		t_obj = pickle.loads(i)
-		json_transactions.append(json.loads(transaction_to_json(t_obj)))
-	print("RUN:\n\n\n")
-	print(json.dumps({"block_hash": block_obj.block_hash.decode('UTF-8'), "height": block_obj.height,
-	"coinbase_transaction": json.loads(coinbase_json), 
-	"transaction_fees": block_obj.transaction_fees, "block_reward": block_obj.block_reward, "total_output": block_obj.total_output,
-	"previous_block_hash": block_obj.previous_block_hash.decode('UTF-8'), "merkle_root": block_obj.merkle_root.decode('UTF-8'),
-	"nonce": block_obj.nonce.decode('UTF-8'), "timestamp": str(block_obj.timestamp), "transactions": json_transactions, "miner_public_key": block_obj.miner_public_key.decode('UTF-8')},
-	sort_keys=True, indent=4, separators=(',', ': ')))
 
 #genesis_to_json(test_gen_block)
-block_to_json(test_gen_block)
-block_to_json(test_reg_block)
-block_to_json(test_reg_2_block)
-block_to_json(test_reg_3_block)
+one = block_to_json(test_gen_block)
+two = block_to_json(test_reg_block)
+three = block_to_json(test_reg_2_block)
+four = block_to_json(test_reg_3_block)
+
+#print(two)
+
+x = json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4, separators=(',', ': '))
+
+t1 = json.loads(two)["transactions"][1]["sender_sig"].encode('latin1')
+t2 = json.loads(two)["transactions"][1]["transaction_data"].encode('latin1')
+
+print(t1)
+print(t2)
+
+
+"""
+
+test_transaction = transaction(sally.serialized_public, sally.serialized_public, 5, 2)
+
+sally.create_transaction(sally.serialized_public, 5, 2)
+
+print(sally.transaction_pool[0])
+
+"""
+print(alice.public_key.verify(t1, t2, ec.ECDSA(hashes.SHA256())))
